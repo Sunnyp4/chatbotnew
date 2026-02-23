33576c338e419fe120a0a29b3b9ad2aafb334424
sunnyp4 <sunnyprakash235@gmail.com>
2026-02-23 20:40:41 +0530

    Initial commit: MERN ChatBot (client + backend)2

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -3,7 +3,10 @@ const cors=require('cors');
 require('dotenv').config()
 const chatRoutes=require('./routes/chat.routes');
 const app=express();
-app.use(cors())
+app.use(cors({
+  origin: ["http://localhost:3000", process.env.CLIENT_URL],
+  credentials: true
+}));
 app.use(express.json());
 app.use('/api',chatRoutes)
 
